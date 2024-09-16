9bf1141c1eb110ea9deb25c749308dc8e6fe0c96
vitxivito <vitoria_garcia_costa_m-tsapp1ma2223@cev.com>
2024-09-16 19:45:12 +0200

    Initial Commit

---
+++ FfCharacters/FfCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FfCharactersApp.swift
+//  FfCharacters
+//
+//  Created by Vitoria Garcia on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FfCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
